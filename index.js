61e53f419b93e2c5ff0371b36adcd78f2d7f4f74
Ashutosh <ashulaley@gmail.com>
2023-02-23 00:51:44 +0530

    file for routes added in routes folder

--- index.js
+++ index.js
@@ -3,11 +3,16 @@ const port = 8000;
 
 const app = express();
 
+
+//use express routes
+app.set('/', require('./routes'));
+
+
 app.listen(port,function(err){
     if(err){
         console.log(`Error in running the server: ${err}`);
     }
-
     
-    console.log(`server is up and running on port: ${port}`);    
-})
+    console.log(`server is up and running on port: ${port}`); 
+   
+});
