4f030aa704d8d3598a141aec1a47d6e49a0d74c9
Ashutosh <ashulaley@gmail.com>
2023-02-26 21:47:02 +0530

    Created basic Layouts and Partials

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const expressEjsLayouts = require('express-ejs-layouts');
 const port = 8000;
 
 const app = express();
@@ -7,6 +8,14 @@ const app = express();
 //set view engine
 app.set('view engine','ejs');
 app.set('views','./views');
+
+//extract styles and scripts written in ejs files.
+app.set('layout extractStyles',true);
+app.set('layout extractScripts',true);
+
+app.use(express.static('./assets'));
+
+app.use(expressEjsLayouts);
 
 //use express routes
 app.use('/', require('./routes'));
